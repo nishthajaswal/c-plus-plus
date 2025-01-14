26144c138b105fd69fba2d755f2c42fd8e428cff
nishthajaswal <nishthajaswal80@gmail.com>
2025-01-14 22:42:30 +0530

    Add files via upload

---
+++ addition_of_two_numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter any number:";
+    cin>>a;
+    cout<<"enter any number:";
+    cin>>b;
+    cout<<"the sum of two numbers is:"<<a+b;
+}
