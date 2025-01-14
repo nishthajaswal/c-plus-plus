596dd59b0d1a5e567cf0fbdb1365edfc82bfdbe1
nishthajaswal <nishthajaswal80@gmail.com>
2025-01-14 22:52:22 +0530

    Add files via upload

---
+++ add of two numbers using class.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class ABC{
+    private:
+    int num1,num2;
+    public:
+    void XYZ(){
+    cout<<"enter any number:";
+    cin>>num1;
+    cout<<"enter any number:";
+    cin>>num2;
+    cout<<"addition of two numbers is:"<<num1+num2<<endl;
+    }
+};
+int main()
+{
+    ABC obj;
+    obj.XYZ();
+    return 0;
+}
